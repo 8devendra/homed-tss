f82cb5729fb4aad99d9602bab05d4d99955882be
8devendra <devendra6969@gmail.com>
2021-11-07 16:42:39 +0530

    abba

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCrb4TQpY_rbvizxDKiHKkosLgC4OfYmjM",
+    authDomain: "login2-bae54.firebaseapp.com",
+    databaseURL: "https://login2-bae54.firebaseio.com",
+    projectId: "login2-bae54",
+    storageBucket: "login2-bae54.appspot.com",
+    messagingSenderId: "913661129215",
+    appId: "1:913661129215:web:fa179bb5f9204070b6b7ca"
+  }
 };
